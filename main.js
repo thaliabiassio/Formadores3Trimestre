3bbe0f635b56cbdf5067dac64a48f656ba2ea30e
thaliabiassio <125232438+thaliabiassio@users.noreply.github.com>
2023-11-29 15:13:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function tocaSom(idElementoAudio){
+ const elemento = document.querySelector(idElementoAudio);
+ if (elemento && elemento.localName === 'audio'){
+    elemento.play();
+ }else{ console.log("Elemento não encontrado ou seletor inválido")
+
+}
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`;
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+tecla.onkeydown = function (evento){
+    if (evento.code == 'Space' || evento.code === 'Enter'){
+    tecla.classList.add('ativa');
+}
+}
+tecla.onkeyup = function (){
+    tecla.classList.remove('ativa');
+}
+}
